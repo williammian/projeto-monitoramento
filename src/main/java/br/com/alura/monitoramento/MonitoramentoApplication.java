223f72ea357af53bbc666281941ac8b0c59873e5
William Mian <william_mian@yahoo.com.br>
2020-12-15 11:19:39 -0300

    implementando segurança ao acessar endpoint do actuator

--- src/main/java/br/com/alura/monitoramento/MonitoramentoApplication.java
+++ src/main/java/br/com/alura/monitoramento/MonitoramentoApplication.java
@@ -3,12 +3,15 @@ package br.com.alura.monitoramento;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
+import br.com.alura.monitoramento.config.security.SecurityConfigurations;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@Import({SecurityConfigurations.class})
 public class MonitoramentoApplication {
 
 	public static void main(String[] args) {
